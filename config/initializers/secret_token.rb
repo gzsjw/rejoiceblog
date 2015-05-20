58fadd89682886f4c501fe5217024e9a181836d2
gzsujw <gzsujw@gmail.com>
2015-05-21 02:08:08 +0800

    2015-05-21 01

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rejoiceblog::Application.config.secret_key_base = 'f366f98511b7f9aae9676840a94fe6b5c26a65b20674615b389dd5dfaf84ed842551e76af22d72e2fb6b3ab69202df9d00a82338eb57a088d4aa8c2f9b86d223'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_RejoiceBlog_session'
+Rejoiceblog::Application.config.session_store :cookie_store, key: '_rejoiceblog_session'
